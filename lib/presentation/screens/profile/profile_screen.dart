fd3a91c67c3221aebc18bf11f9006c4dfd6f873e
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2025-07-27 18:53:06 +0100

    feat: Refactor user profile screen to improve layout and modularize user info display

--- lib/presentation/screens/profile/profile_screen.dart
+++ lib/presentation/screens/profile/profile_screen.dart
@@ -52,7 +52,7 @@ class UserProfileScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 CircleAvatar(
-                  radius: 55,
+                  radius: 45,
                   backgroundColor: Colors.grey.shade200,
                   backgroundImage:
                       user.imageUser != null
@@ -62,7 +62,7 @@ class UserProfileScreen extends StatelessWidget {
                       user.imageUser == null
                           ? const Icon(
                             Icons.person,
-                            size: 50,
+                            size: 40,
                             color: Colors.grey,
                           )
                           : null,
@@ -81,53 +81,14 @@ class UserProfileScreen extends StatelessWidget {
                   user.email,
                   style: const TextStyle(fontSize: 14, color: Colors.black54),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 10),
 
-                Card(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Column(
-                    children: [
-                      _buildInfoTile(
-                        Icons.phone,
-                        "phone_number".tr,
-                        user.phone ?? "unavailable".tr,
-                      ),
-                      _buildInfoTile(
-                        Icons.home,
-                        'address'.tr,
-                        user.address ?? "unavailable".tr,
-                      ),
-                      _buildInfoTile(
-                        Icons.verified_user,
-                        "email_verified".tr,
-                        user.emailVerifiedAt != null ? 'yes'.tr : 'no'.tr,
-                      ),
-                      _buildInfoTile(
-                        Icons.calendar_today,
-                        "creation_date".tr,
-                        user.createdAt != null
-                            ? user.createdAt!.split('T').first
-                            : "unavailable".tr,
-                      ),
-                      _buildInfoTile(
-                        Icons.access_time_filled,
-                        "expiration_date".tr,
-                        expiryDate != null
-                            ? dateFormat.format(expiryDate)
-                            : "unavailable".tr,
-                        trailing: Chip(
-                          label: Text(
-                            isExpired ? "expired".tr : "valid".tr,
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                          backgroundColor:
-                              isExpired ? Colors.red : Colors.green,
-                        ),
-                      ),
-                    ],
-                  ),
+                buildUserInfoSection(
+                  user: user,
+                  dateFormat: dateFormat,
+                  expiryDate: expiryDate,
+                  isExpired: isExpired,
                 ),
-
                 const SizedBox(height: 12),
 
                 /// زر تسجيل الخروج
@@ -157,16 +118,79 @@ class UserProfileScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildInfoTile(
-    IconData icon,
-    String title,
-    String subtitle, {
+  Widget buildUserInfoSection({
+    required user,
+    required DateTime? expiryDate,
+    required bool isExpired,
+    required DateFormat dateFormat,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _buildCardInfoTile(
+          icon: Icons.phone,
+          title: "phone_number".tr,
+          value: user.phone ?? "unavailable".tr,
+        ),
+        _buildCardInfoTile(
+          icon: Icons.home,
+          title: "address".tr,
+          value: user.address ?? "unavailable".tr,
+        ),
+        _buildCardInfoTile(
+          icon: Icons.verified_user,
+          title: "email_verified".tr,
+          value: user.emailVerifiedAt != null ? "yes".tr : "no".tr,
+          iconColor: user.emailVerifiedAt != null ? Colors.green : Colors.red,
+        ),
+        _buildCardInfoTile(
+          icon: Icons.calendar_today,
+          title: "creation_date".tr,
+          value:
+              user.createdAt != null
+                  ? user.createdAt!.split('T').first
+                  : "unavailable".tr,
+        ),
+        _buildCardInfoTile(
+          icon: Icons.access_time_filled,
+          title: "expiration_date".tr,
+          value:
+              expiryDate != null
+                  ? dateFormat.format(expiryDate)
+                  : "unavailable".tr,
+          trailing: Chip(
+            label: Text(
+              isExpired ? "expired".tr : "valid".tr,
+              style: const TextStyle(color: Colors.white),
+            ),
+            backgroundColor: isExpired ? Colors.red : Colors.green,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCardInfoTile({
+    required IconData icon,
+    required String title,
+    required String value,
+    Color iconColor = AppColors.primary,
     Widget? trailing,
   }) {
     return ListTile(
-      leading: Icon(icon, color: AppColors.primary),
-      title: Text(title),
-      subtitle: Text(subtitle),
+      leading: Icon(icon, color: iconColor, size: 28),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+          color: AppColors.primary,
+        ),
+      ),
+      subtitle: Text(
+        value,
+        style: const TextStyle(fontSize: 14, color: AppColors.primary),
+      ),
       trailing: trailing,
     );
   }
