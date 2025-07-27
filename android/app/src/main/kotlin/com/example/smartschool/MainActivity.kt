2aa0220a1eed1755be03bfd399e4c1ab99d3886f
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2025-07-27 20:18:28 +0100

    refactor: Update package name and namespace to 'com.houssamaziez.cosmolearn'

--- android/app/src/main/kotlin/com/example/smartschool/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/smartschool/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.houssamaziez.Learncosmeticacademy
+package com.houssamaziez.cosmolearn
 
 import android.os.Bundle
 import android.view.WindowManager
@@ -15,7 +15,7 @@ class MainActivity : FlutterActivity() {
         )
     }
 }
-// package com.houssamaziez.Learncosmeticacademy
+// package com.houssamaziez.cosmolearn
 
 // import io.flutter.embedding.android.FlutterActivity
 
